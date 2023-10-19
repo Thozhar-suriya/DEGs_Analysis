4a4f00f93886f397ae01767d1209b977053bd363
Vigneshwar Suriya Prakash S <50394703+Thozhar-suriya@users.noreply.github.com>
2023-10-19 16:07:13 +0530

    Update DEseq.R

--- DEseq.R
+++ DEseq.R
@@ -76,39 +76,44 @@ A <- merge(df, G_list, by = "ensembl_gene_id")
 write.csv(A, "PRJNA435914_Korean_Male_UpandDown_withsymbol.csv")
 
 ### Volcano Plot ####
-library(EnhancedVolcano)
-keyvals <- ifelse(
-res$log2FoldChange < -1.5, 'green',
-ifelse(res$log2FoldChange > 1.5, 'red',
-'gray'))
-keyvals[is.na(keyvals)] <- 'gray'
-names(keyvals)[keyvals == 'red'] <- 'High'
-names(keyvals)[keyvals == 'gray'] <- 'Normal'
-names(keyvals)[keyvals == 'green'] <- 'Low'
-EnhancedVolcano(res,
-lab = rownames(res),
-x = 'log2FoldChange',
-y = 'pvalue',
-selectLab = rownames(res)[which(names(keyvals) %in% c('high', 'low'))],
-axisLabSize = 12,
-ylim = c(0, 40),
-xlim = c(-6, 6),
-cutoffLineType = 'blank',
-transcriptPointSize = 1.5,
-transcriptLabSize = FALSE,
-caption = NULL,
-captionLabSize = 12,
-title = 'Volcano plot',
-subtitle = 'Differential Expression (PRJNA435914)',
-colCustom = keyvals,
-legend = c("NA", "Up-regulated", "Down-regulated"),
-legendPosition = "top",
-hline = NULL,
-vline = NULL,
-pCutoff = 0.05,
-FCcutoff = 2,
-gridlines.major = FALSE,
-gridlines.minor = FALSE)
+library(forcats)
+data <- read.csv("DEGs_UpandDown1.csv", header = TRUE, row.names = 1)
+data <- data %>%
+  mutate(gene_type = case_when(log2FoldChange >= 1 & pvalue <= 0.05 ~ "up",
+                               log2FoldChange <= -1 & pvalue <= 0.05 ~ "down",
+                               TRUE ~ "ns"))   
+data %>%
+  count(gene_type) %>%
+  knitr::kable()
+
+data <- data %>%
+  mutate(gene_type = fct_relevel(gene_type, "up", "down")) 
+
+data %>%
+  distinct(gene_type) %>%
+  pull()
+
+cols <- c("up" = "red", "down" = "green", "ns" = "grey") 
+sizes <- c("up" = 2, "down" = 2, "ns" = 1) 
+alphas <- c("up" = 1, "down" = 1, "ns" = 0.5)
+
+data %>%
+  ggplot(aes(x = log2FoldChange,
+             y = -log10(pvalue),
+             fill = gene_type,
+             size = gene_type,
+             alpha = gene_type)) + 
+  geom_point(shape = 21, # Specify shape and colour as fixed local parameters    
+             colour = "grey") + 
+  geom_hline(yintercept = -log10(0.05),
+             #linetype = "dashed") + 
+  geom_vline(xintercept = c(log2(0.5), log2(2)),
+             #linetype = "dashed") +
+  scale_fill_manual(values = cols) + # Modify point colour
+  scale_size_manual(values = sizes) + # Modify point size
+  scale_alpha_manual(values = alphas) + # Modify point transparency
+  scale_x_continuous(breaks = c(seq(-5, 5, 1)),  
+                     limits = c(-3, 3))  
 
 ## Heatmap ##
 
